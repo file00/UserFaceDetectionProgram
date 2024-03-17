8ae9f342e3116f13623e93be70e2b5b2286e379b
file00 <51076009+file00@users.noreply.github.com>
2024-03-17 14:58:34 +0900

    Update TakePhoto.py

--- TakePhoto.py
+++ TakePhoto.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_classifier = cv2.CascadeClassifier('xml.File')
 
 def face_extractor(img):
 
@@ -27,7 +27,7 @@ while True:
         face = cv2.resize(face_extractor(frame),(200,200))
         face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
 
-        file_name_path = 'User Detection/Faces/User'+str(count)+'.jpg'
+        file_name_path = 'File_Path'+str(count)+'.jpg'
         cv2.imwrite(file_name_path,face)
 
         cv2.putText(face,str(count),(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
